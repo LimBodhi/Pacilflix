508c52097bdca8ed93b504fcfbf597c2113c5589
azmifal <kunazmifalah@gmail.com>
2024-05-02 22:11:02 +0700

    halaman tayangan

--- Tayangan/views.py
+++ Tayangan/views.py
@@ -1,7 +1,3 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.shortcuts import render
 from django.http import JsonResponse
 from utils.query import query
 from django.shortcuts import render, redirect
