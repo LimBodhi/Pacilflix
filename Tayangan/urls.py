31df084eec1122e5ac40080bfd517a0c7912f89c
Humam Al Labib <humam.alabib@gmail.com>
2024-05-02 20:06:45 +0700

    halaman halaman belum selesai

--- Tayangan/urls.py
+++ Tayangan/urls.py
@@ -6,4 +6,7 @@ apps = 'Tayangan'
 urlpatterns = [
     path('', show_tayangan, name='show_tayangan'),
     path('search/', show_search, name='show_search'),
+    path('film/', show_film, name='show_film'),
+    path('series/', show_series, name='show_series'),
+    path('episode/', show_episode, name='show_episode'),
 ]
